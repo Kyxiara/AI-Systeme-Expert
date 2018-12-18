8a02792ceffd4d7d2f48670f4609a4103b26cac2
Cedric Caballol <cedric.caballol@hotmail.fr>
2018-12-18 09:45:14 +0100

    mardi matin UNFAILED

--- TP.lisp
+++ TP.lisp
@@ -7,18 +7,12 @@
 
 (defun trouveReglesActivables (listeRegles listeReglesActivables)
   (cond ((atom listeRegles) listeReglesActivables)
-<<<<<<< HEAD
         ((eval (regle-condition (car listeRegles))) (trouveReglesActivables (cdr listeRegles) (cons (car listeRegles) listeReglesActivables)))
         (t (trouveReglesActivables (cdr listeRegles) listeReglesActivables))
-=======
-        ((regle-condition (car listeRegles)) (append (car listeRegles) listeReglesActivables) (trouveReglesActivables (cdr listeRegles) listeReglesActivables))
-        (t (membrep l1 (cdr l2)))
->>>>>>> master
   )
 )
 
 (defun compareRegleGT (r1 r2)
-<<<<<<< HEAD
     (cond ((> (regle-poids r1) (regle-poids r2)) nil)
           (t t)
     )
@@ -29,14 +23,6 @@
         ((equal (regle-nom element) (regle-nom (car liste))) (retireElementListe element (cdr liste)))
         (T (cons (car liste) (retireElementListe element (cdr liste))))))
   
-=======
-    (cond ((> (regle-poids r1) (regle-poids r2)) r1)
-          (t r2)
-    )
-)
-
-    
->>>>>>> master
 
 ;------------------------------------------
     
@@ -115,7 +101,6 @@
     action
 )
 
-<<<<<<< HEAD
 (setq regleAdministrationVentoline (make-regle
   :nom "regle administration Ventoline"
   :poids 1
@@ -167,65 +152,7 @@
    (setq regle (car liste_regles_en_conflit))
    (print (regle-action regle))
    (retireElementListe regle liste_regles)
-=======
-(setq regle1 (make-regle
-  :nom "Regle 1"
-  :poids 1
-  :condition (membrep 'asthme '(liste_pathologies))
-  :action ventoline )
 )
 
-(setq regle2 (make-regle
-  :nom "Regle 2"
-  :poids 2
-  :condition 'difficultes_respiratoire
-  :action (cons 'asthme '(liste_pathologies)) )
-)
-
-(setq regle3 (make-regle
-  :nom "Regle 3"
-  :poids 3
-  :condition 'rougeur
-  :action (cons 'inflammation '(liste_pathologies)) )
->>>>>>> master
-)
-;------------------------------------------
-
-; Symptomes disponibles : rougeur gonflement sensationChaleur difficultesRespiratoire
-; Base de faits :
-;(setq liste_symptomes ('difficultesRespiratoire))
-(setq liste_pathologies ())
-(setq liste_medicaments ())
-(setq liste_regles (list regle3 regle2 regle1))
-(setq liste_regles_en_conflit ())
-
-    
-; Moteur d'inférence
-
-(setq ARRET nil)
-
-(loop
-   ; Vérification de la condition d'arret
-    (when '(not ARRET) (return nil))
-   ; Détermination de l'ensemble des conflits
-     
-   ; On trie la liste des conflits en fonction du poid des règles
-     ;TODO
-   ; On effectue l'action associé à la règle en tète de liste
-     ;TODO
-   
-)
-
-
-;(print (compareRegleGT regle1 regle2))
-(print (sort liste_regles
-      (lambda (x y) (< (regle-poids x) (regle-poids y)))))
-
-<<<<<<< HEAD
 (print "DONE")
 (print (eval (regle-condition regleDiagnosticAsthme)))
-=======
-;(print (sort liste_regles #'compareRegleGT))
-;(print liste_regles)
-;(print (eval (regle-nom (compareregleGT )))
->>>>>>> master
